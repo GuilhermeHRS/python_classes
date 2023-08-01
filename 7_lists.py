5c0cbd8fe243bcb196c62028bb4b3c09c748e700
GuilhermeHRS <guilhermehsantos2412@gmail.com>
2023-08-01 16:44:51 -0300

    Adding some Python Lists + functions

--- 7_lists.py
+++ 7_lists.py
@@ -1,5 +1,17 @@
 names = ["Guilherme", "Gabriel", "Davi"]
 # print(nmes)
 
-for name in names:
-    print(name.upper()) 
+# for name in names:
+#     print(name.upper()) 
+
+# if "Guilherme" in names:
+#     names.remove("Gabriel")
+
+#     names.insert(3, "Robinho")
+#     names.append("Gabriel")
+#     print(names)
+
+names2 = ["Maria","Julia","Carol"]
+names.extend(names2)
+
+# print(names) - ['Guilherme', 'Gabriel', 'Davi', 'Maria', 'Julia', 'Carol']
--- python_exercises/4_practice_questions.py
+++ python_exercises/4_practice_questions.py
@@ -36,16 +36,33 @@
 # else:
 #     print("Par!")
 
-#or
+#or --------
 
-def check_even_odd(number):
-    if number % 2 == 0:
-        print(f"{number} é um número par")
-    else:
-        print(f"{number} é um número ímpar")
+# def check_even_odd(number):
+#     if number % 2 == 0:
+#         print(f"{number} é um número par")
+#     else:
+#         print(f"{number} é um número ímpar")
 
-try:
-    user_input = int(input("Escreva um número: "))
-    check_even_odd(user_input)
-except ValueError:
-    print("Número inválido. Por gentileza insira um número válido!")
+# try:
+#     user_input = int(input("Escreva um número: "))
+#     check_even_odd(user_input)
+# except ValueError:
+#     print("Número inválido. Por gentileza insira um número válido!")
+
+#4 - Given a list of numbers, find the maximum and minimum values.
+
+# list_numbers = [0,1,2,3,4,5,6,7,8,9,10]
+# min = min(list_numbers)
+# max = max(list_numbers)
+
+# print(f"Menor número {min}\nMaior número {max}")
+
+#5 - Create a Python function to check if a given string is a palindrome
+
+# def is_palindrome():
+    #Remove spaces and convert to lowercase for case-insensitive comparison
+
+#---------------------------- I have to do late 
+
+#6 - Calculate the compound interest for a given principal amount, interest rate, and time period
